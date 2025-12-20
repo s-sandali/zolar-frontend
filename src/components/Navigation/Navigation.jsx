d3117b7518c3f6b91f28242864aa5cea980334fd
Sandali.js <sandalisandagomi@gmail.com>
2025-12-21 00:55:57 +0530

    fix:made some ui changes to the hero page and navr bar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -14,29 +14,13 @@ const Navigation = () => {
   return (
     <nav className={"px-12 py-6 flex justify-between items-center"}>
       <Link to="/" className={"flex items-center gap-3"}>
-        <div
-          className={
-            "w-10 h-10 rounded-full bg-lime-400 flex justify-center items-center"
-          }
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-wind-icon lucide-wind logo block"
-          >
-            <path d="M12.8 19.6A2 2 0 1 0 14 16H2" />
-            <path d="M17.5 8a2.5 2.5 0 1 1 2 4H2" />
-            <path d="M9.8 4.4A2 2 0 1 1 11 8H2" />
-          </svg>
+        <div className="flex items-center justify-center">
+          <img
+            src="/assets/images/zolar-logo.png"
+            alt="Zolar logo"
+            className="h-40 w-auto"
+          />
         </div>
-        <span className="font-[Inter] text-xl font-semibold">Aelora</span>
       </Link>
 
       <div className={"flex items-center gap-12"}>
