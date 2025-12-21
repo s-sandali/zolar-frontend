5e6bbe08885e3c4b4ae9e2b452c20305bec055d8
Sandali.js <sandalisandagomi@gmail.com>
2025-12-21 18:18:38 +0530

    feat: update HomePage component to include additional sections for enhanced user experience

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,9 +1,19 @@
 import HeroSection from "./components/HeroSection";
+import EnergyOverviewSection from "./components/EnergyOverviewSection";
+import ProblemSection from "./components/ProblemSection";
+import SolutionSection from "./components/SolutionSection";
+import FeaturesSection from "./components/FeaturesSection";
+import HomeFooter from "./components/HomeFooter";
 
 const HomePage = () => {
   return (
     <main>
       <HeroSection />
+      <EnergyOverviewSection />
+      <ProblemSection />
+      <SolutionSection />
+      <FeaturesSection />
+      <HomeFooter />
     </main>
   );
 };
