ded85b9257bbd1e65b6b0d60a0be47310aaa7261
Sandali.js <sandalisandagomi@gmail.com>
2025-12-22 12:20:49 +0530

    feat: refactor MainLayout to improve structure and enhance layout consistency

--- src/layouts/main.layout.jsx
+++ src/layouts/main.layout.jsx
@@ -3,9 +3,11 @@ import { Outlet } from "react-router";
 
 export default function MainLayout() {
   return (
-    <>
+    <div className="relative isolate min-h-screen bg-gradient-to-b from-background via-background via-background/95 to-background">
       <Navigation />
-      <Outlet />
-    </>
+      <main className="mx-auto flex w-full max-w-6xl flex-1 flex-col px-4 pb-16 pt-10 sm:px-6 lg:px-8">
+        <Outlet />
+      </main>
+    </div>
   );
-};
+}
