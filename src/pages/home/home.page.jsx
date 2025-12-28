bc3171ac90bf24040d4668eb6fe54d814324da34
Sandali.js <sandalisandagomi@gmail.com>
2025-12-28 20:55:02 +0530

    feat: add TrustSignalsSection to HomePage and remove FeaturesSection

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,8 +1,8 @@
 import HeroSection from "./components/HeroSection";
+import TrustSignalsSection from "./components/TrustSignalsSection";
 import EnergyOverviewSection from "./components/EnergyOverviewSection";
 import ProblemSection from "./components/ProblemSection";
 import SolutionSection from "./components/SolutionSection";
-import FeaturesSection from "./components/FeaturesSection";
 import HomeFooter from "./components/HomeFooter";
 import ScrollReveal from "./components/ScrollReveal";
 
@@ -10,6 +10,7 @@ const HomePage = () => {
   return (
     <main>
       <HeroSection />
+      <TrustSignalsSection />
       <ScrollReveal>
         <section id="about">
           <EnergyOverviewSection />
@@ -25,11 +26,7 @@ const HomePage = () => {
           <SolutionSection />
         </section>
       </ScrollReveal>
-      <ScrollReveal delay={0.1}>
-        <section id="features">
-          <FeaturesSection />
-        </section>
-      </ScrollReveal>
+     
       <ScrollReveal delay={0.1}>
         <HomeFooter />
       </ScrollReveal>
